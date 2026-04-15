918ef1433c11f17941824aed36bdf6371fa968c8
prosekmiha <prosekmiha@gmail.com>
2026-04-15 20:58:24 +0200

    fix/sitemap-file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { getAllArticles } from "@/lib/articles";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const articles = getAllArticles();
 
